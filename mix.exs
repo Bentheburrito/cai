f23b2fbcc8d31d587bc1035aed269bfedef911d7
Ben <lolitsaburrito@gmail.com>
2023-06-23 14:53:04 -0700

    all da dependencies

--- mix.exs
+++ mix.exs
@@ -47,7 +47,11 @@ defmodule CAI.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:planetside_api, "~> 0.3.0"},
+      {:dotenv_parser, "~> 1.2"},
+      {:cachex, "~> 3.6"},
+      {:credo, "~> 1.6", only: [:dev, :test, :github], runtime: false},
     ]
   end
 
