0af872b1d2d847147a903d3f624858f16cf34d56
Bentheburrito <lolitsaburrito@gmail.com>
2023-07-05 20:01:21 -0700

    Scripts: filter out old HIVE XP types from static data

--- lib/cai/scripts.ex
+++ lib/cai/scripts.ex
@@ -127,7 +127,9 @@ defmodule CAI.Scripts do
       PS2.API.query(Query.new(collection: "experience") |> limit(5000), "example")
 
     new_xp_map =
-      for xp_map <- xp_list, into: %{} do
+      for %{"description" => desc} = xp_map <- xp_list,
+          not String.contains?(desc, "HIVE"),
+          into: %{} do
         xp_map
         |> Map.update("xp", 0, to_int_or_float)
         |> Map.pop!("experience_id")
