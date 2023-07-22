99e7934c7ea7171ab0f5b89619593f4b3e15e346
Bentheburrito <lolitsaburrito@gmail.com>
2023-07-22 15:37:51 -0700

    init CI setup 🤞 tests pass

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -57,4 +57,101 @@ defmodule CAI.DataCase do
       end)
     end)
   end
+
+  @doc """
+  Insert 3 Death events and their corresponding "Kill Player" GEs for the given character_id, where the first 2 are
+  kills and the last is a death.
+  """
+  def insert_kills(character_id, base_timestamp) do
+    deaths = [
+      %{
+        character_id: 5_429_413_765_549_775_185,
+        timestamp: base_timestamp,
+        attacker_character_id: character_id,
+        attacker_fire_mode_id: 70_019,
+        attacker_loadout_id: 6,
+        attacker_team_id: 2,
+        attacker_vehicle_id: 0,
+        attacker_weapon_id: 803_699,
+        character_loadout_id: 32,
+        is_critical: false,
+        is_headshot: false,
+        team_id: 2,
+        world_id: 13,
+        zone_id: 4
+      },
+      %{
+        character_id: 8_279_743_051_027_650_625,
+        timestamp: base_timestamp + 5,
+        attacker_character_id: character_id,
+        attacker_fire_mode_id: 70_019,
+        attacker_loadout_id: 6,
+        attacker_team_id: 2,
+        attacker_vehicle_id: 0,
+        attacker_weapon_id: 803_699,
+        character_loadout_id: 15,
+        is_critical: false,
+        is_headshot: false,
+        team_id: 2,
+        world_id: 13,
+        zone_id: 4
+      },
+      %{
+        character_id: character_id,
+        timestamp: base_timestamp + 6,
+        attacker_character_id: 5_429_413_765_549_775_185,
+        attacker_fire_mode_id: 7572,
+        attacker_loadout_id: 20,
+        attacker_team_id: 1,
+        attacker_vehicle_id: 0,
+        attacker_weapon_id: 7274,
+        character_loadout_id: 32,
+        is_critical: false,
+        is_headshot: false,
+        team_id: 1,
+        world_id: 13,
+        zone_id: 4
+      }
+    ]
+
+    CAI.Repo.insert_all(CAI.ESS.Death, deaths)
+
+    ges = [
+      %{
+        amount: 100,
+        character_id: character_id,
+        experience_id: 1,
+        loadout_id: 32,
+        other_id: 5_429_413_765_549_775_185,
+        team_id: 2,
+        timestamp: base_timestamp,
+        world_id: 13,
+        zone_id: 4
+      },
+      %{
+        amount: 100,
+        character_id: character_id,
+        experience_id: 1,
+        loadout_id: 32,
+        other_id: 8_279_743_051_027_650_625,
+        team_id: 2,
+        timestamp: base_timestamp + 5,
+        world_id: 13,
+        zone_id: 4
+      },
+      %{
+        amount: 100,
+        character_id: 5_429_413_765_549_775_185,
+        experience_id: 1,
+        loadout_id: 20,
+        other_id: character_id,
+        team_id: 1,
+        timestamp: base_timestamp + 6,
+        world_id: 13,
+        zone_id: 4
+      }
+    ]
+
+    CAI.Repo.insert_all(CAI.ESS.GainExperience, ges)
+  end
 end
